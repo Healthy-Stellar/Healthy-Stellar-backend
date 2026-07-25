56a371b00cd8e6b1c4f3ff45e9d8a728f12aca93
James Akolo <jamesjambox@gmail.com>
2026-07-25 18:36:43 +0100

    fix: register AppointmentsModule, SurgicalModule, and TelemedicineModule in AppModule (#821)

--- src/app.module.ts
+++ src/app.module.ts
@@ -84,6 +84,9 @@ import { PiiRedactionInterceptor } from './common/interceptors/pii-redaction.int
 import { BedOccupancyModule } from './bed-occupancy/bed-occupancy.module';
 import { MedicalStaffModule } from './medical-staff/medical-staff.module';
 import { HealthcareMonitoringModule } from './healthcare-monitoring/healthcare-monitoring.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { SurgicalModule } from './surgical-management-system/surgical/Surgical.module';
+import { TelemedicineModule } from './telemedicine-and-remote/src/telemedicine/Telemedicine.module';
 import { User } from './auth/entities/user.entity';
 
 @Module({
@@ -175,6 +178,9 @@ import { User } from './auth/entities/user.entity';
     MedicalStaffModule,
     EhrImportModule,
     HealthcareMonitoringModule,
+    AppointmentsModule,
+    SurgicalModule,
+    TelemedicineModule,
     EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
